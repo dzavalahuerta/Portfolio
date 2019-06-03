efb878756d6d6286923a6c5206f2a2c6f5abb004
DanielZavalaHuerta <danielz123789@gmail.com>
2019-06-02 18:00:02 -0700

    added line animation to links in the navbar, added the animation spin in animation for the links in the main-content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-content',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
+  @ViewChild('nasaProjectLink') nasaProjectLink;
 
   constructor() { }
 
   ngOnInit() {
-  }
+    let nasaProjectLinkOffset = this.nasaProjectLink.nativeElement.offsetTop/12;
+    let document = window.document;
 
+    document.addEventListener('scroll',()=>{
+      if(window.pageYOffset > nasaProjectLinkOffset && this.nasaProjectLink.nativeElement.className === 'hidden'){
+        console.log('it worked');
+        this.nasaProjectLink.nativeElement.classList.remove("hidden");
+      }
+    });
+  }
 }
