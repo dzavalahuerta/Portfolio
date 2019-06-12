420095abeab6d7aec6a4a57f0f5983c9ff946ad7
DanielZavalaHuerta <danielz123789@gmail.com>
2019-06-12 02:20:47 -0700

    finzalized the links in the navbar and footer, added the zillow-clone project and made the site responsive

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -7,18 +7,24 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
   @ViewChild('nasaProjectLink') nasaProjectLink;
+  @ViewChild('zillowCloneLink') zillowCloneLink;
 
   constructor() { }
 
   ngOnInit() {
     let nasaProjectLinkOffset = this.nasaProjectLink.nativeElement.offsetTop/12;
-    let document = window.document;
+    let zillowCloneLinkOffset = this.zillowCloneLink.nativeElement.offsetTop/2;
 
-    document.addEventListener('scroll',()=>{
+    window.addEventListener('scroll',()=>{
+      console.log(window.pageYOffset);
       if(window.pageYOffset > nasaProjectLinkOffset && this.nasaProjectLink.nativeElement.className === 'hidden'){
-        console.log('it worked');
         this.nasaProjectLink.nativeElement.classList.remove("hidden");
       }
+      if(window.pageYOffset > zillowCloneLinkOffset && this.zillowCloneLink.nativeElement.className === 'hidden'){
+        this.zillowCloneLink.nativeElement.classList.remove("hidden");
+      }
     });
+    console.log(this.zillowCloneLink);
+    console.log(zillowCloneLinkOffset);
   }
 }
