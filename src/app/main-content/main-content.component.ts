b55875846da319c000782d8a7df344baebd87ed4
DanielZavalaHuerta <danielz123789@gmail.com>
2019-06-14 14:29:32 -0700

    made the animation show up on init for nasa-project instead of on scroll and made the animation for the navbar links wider

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -12,19 +12,15 @@ export class MainContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    let nasaProjectLinkOffset = this.nasaProjectLink.nativeElement.offsetTop/12;
     let zillowCloneLinkOffset = this.zillowCloneLink.nativeElement.offsetTop/2;
+    setTimeout(() => {
+      this.nasaProjectLink.nativeElement.classList.remove("hidden");
+    }, 500);
 
     window.addEventListener('scroll',()=>{
-      console.log(window.pageYOffset);
-      if(window.pageYOffset > nasaProjectLinkOffset && this.nasaProjectLink.nativeElement.className === 'hidden'){
-        this.nasaProjectLink.nativeElement.classList.remove("hidden");
-      }
       if(window.pageYOffset > zillowCloneLinkOffset && this.zillowCloneLink.nativeElement.className === 'hidden'){
         this.zillowCloneLink.nativeElement.classList.remove("hidden");
       }
     });
-    console.log(this.zillowCloneLink);
-    console.log(zillowCloneLinkOffset);
   }
 }
