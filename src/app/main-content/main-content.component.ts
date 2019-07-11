6da17a0716e45236c8b5c53131703380b268b130
DanielZavalaHuerta <danielz123789@gmail.com>
2019-07-10 17:57:36 -0700

    added div to show details about each project and updated resume

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -12,15 +12,47 @@ export class MainContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    let zillowCloneLinkOffset = this.zillowCloneLink.nativeElement.offsetTop/2;
     setTimeout(() => {
       this.nasaProjectLink.nativeElement.classList.remove("hidden");
     }, 500);
 
+    let zillowCloneLinkOffset = this.zillowCloneLink.nativeElement.offsetTop/2;
     window.addEventListener('scroll',()=>{
-      if(window.pageYOffset > zillowCloneLinkOffset && this.zillowCloneLink.nativeElement.className === 'hidden'){
+      if(window.pageYOffset > zillowCloneLinkOffset
+        && this.zillowCloneLink.nativeElement.className === 'hidden project'){
         this.zillowCloneLink.nativeElement.classList.remove("hidden");
       }
     });
+
+    let allProjects = document.querySelectorAll('a[class="hidden project"]');
+    for (let i = 0; i < allProjects.length; i++) {
+      let projectInfoDiv = allProjects[i].firstElementChild;
+
+      let projectInfoDivChildren: any = projectInfoDiv.children;
+
+      projectInfoDiv.addEventListener('touchstart',()=>{
+        for (let index = 0; index < projectInfoDivChildren.length; index++) {
+          if(projectInfoDivChildren[index] === projectInfoDivChildren[0]){
+            projectInfoDivChildren[index].style.transform = 'scale(1.04)';
+            projectInfoDivChildren[index].style.transitionDuration = '1000ms';
+          }
+          else{
+            projectInfoDivChildren[index].style.display = 'unset';
+          }
+        }
+      });
+      
+      projectInfoDiv.addEventListener('touchend',()=>{
+        for (let index = 0; index < projectInfoDivChildren.length; index++) {
+          if(projectInfoDivChildren[index] === projectInfoDivChildren[0]){
+            projectInfoDivChildren[index].style.transform = '';
+            projectInfoDivChildren[index].style.transitionDuration = '';
+          }
+          else{
+            projectInfoDivChildren[index].style.display = 'none';
+          }
+        }
+      });
+    }
   }
 }
